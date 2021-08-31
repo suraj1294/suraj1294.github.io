63429351dbb7b4453d662a3cc600e290fa815520
spatil_ftdr <suraj.patil@frontdoorhome.com>
2021-08-31 13:50:36 +0530

    add cookie check

--- cookie-test/script.js
+++ cookie-test/script.js
@@ -1,1 +1,12 @@
 console.log("hello from script")
+
+if (window.parent) {
+    debugger;
+	if (/thirdparty=yes/.test(document.cookie)) {
+		window.parent.postMessage('MM:3PCsupported', '*');
+	} else {
+		window.parent.postMessage('MM:3PCunsupported', '*');
+        
+	}
+	document.cookie = 'thirdparty=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+ }
