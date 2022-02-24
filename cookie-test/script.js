f8e0b3cf36800d749e7f6a3f1262cf942e2be8ac
spatil_ftdr <suraj.patil@frontdoorhome.com>
2022-02-24 19:51:18 +0530

    cookie test

--- cookie-test/script.js
+++ cookie-test/script.js
@@ -1,24 +1,15 @@
 console.log("hello from script");
 
-if (
-  typeof window === "undefined" ||
-  typeof window.localStorage === "undefined"
-) {
-  console.log("Please verify cookie settings");
-} else {
-  localStorage.setItem("some1", "test1");
+function lsTest(value) {
+  try {
+    localStorage.setItem("sample", value);
+    localStorage.removeItem("sample");
+    return true;
+  } catch (e) {
+    return false;
+  }
 }
 
-function createTable(rows, cols) {
-  var j = 1;
-  var output = "<table border='1' width='500' cellspacing='0'cellpadding='5'>";
-  for (i = 1; i <= rows; i++) {
-    output = output + "<tr>";
-    while (j <= cols) {
-      output = output + "<td>" + i * j + "</td>";
-      j = j + 1;
-    }
-    output = output + "</tr>";
-    j = 1;
-  }
+if (!lsTest("test")) {
+  console.log("Please verify cookie settings");
 }
