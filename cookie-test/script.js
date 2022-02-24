a2475eb89ba3a8916800012cb2b3619d11265eac
spatil_ftdr <suraj.patil@frontdoorhome.com>
2022-02-24 19:42:07 +0530

    add dummy code

--- cookie-test/script.js
+++ cookie-test/script.js
@@ -1,7 +1,24 @@
 console.log("hello from script");
 
-if (typeof window.localStorage === "undefined") {
+if (
+  typeof window === "undefined" ||
+  typeof window.localStorage === "undefined"
+) {
   console.log("Please verify cookie settings");
 } else {
   localStorage.setItem("some1", "test1");
+}
+
+function createTable(rows, cols) {
+  var j = 1;
+  var output = "<table border='1' width='500' cellspacing='0'cellpadding='5'>";
+  for (i = 1; i <= rows; i++) {
+    output = output + "<tr>";
+    while (j <= cols) {
+      output = output + "<td>" + i * j + "</td>";
+      j = j + 1;
+    }
+    output = output + "</tr>";
+    j = 1;
+  }
 }
