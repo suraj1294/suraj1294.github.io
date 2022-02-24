a1e3d87a6c74ceb4ced85b2c44e552c93411054e
spatil_ftdr <suraj.patil@frontdoorhome.com>
2022-02-24 20:13:46 +0530

    set cookie check

--- cookie-test/script.js
+++ cookie-test/script.js
@@ -3,7 +3,6 @@ console.log("hello from script");
 function lsTest(value) {
   try {
     localStorage.setItem("sample", value);
-    localStorage.removeItem("sample");
     return true;
   } catch (e) {
     return false;
