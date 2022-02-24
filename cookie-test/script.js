557868637e32d0ee5452eb20673a9b940314f81e
spatil_ftdr <suraj.patil@frontdoorhome.com>
2022-02-24 19:05:23 +0530

    cookie check

--- cookie-test/script.js
+++ cookie-test/script.js
@@ -1,16 +1,20 @@
 console.log("hello from script");
 
-window.localStorage.setItem("some", "test");
-
-localStorage.setItem("some1", "test1");
-
-if (window.parent) {
-  debugger;
-  if (/thirdparty=yes/.test(document.cookie)) {
-    window.parent.postMessage("MM:3PCsupported", "*");
-    document.getElementById("result").innerHTML = "supported";
-  } else {
-    window.parent.postMessage("MM:3PCunsupported", "*");
-    document.getElementById("result").innerHTML = "not supported";
-  }
+if (typeof window === "undefined") {
+  console.log("Please verify cookie settings");
+} else {
+  localStorage.setItem("some1", "test1");
 }
+
+//window.localStorage.setItem("some", "test");
+
+// if (window.parent) {
+//   debugger;
+//   if (/thirdparty=yes/.test(document.cookie)) {
+//     window.parent.postMessage("MM:3PCsupported", "*");
+//     document.getElementById("result").innerHTML = "supported";
+//   } else {
+//     window.parent.postMessage("MM:3PCunsupported", "*");
+//     document.getElementById("result").innerHTML = "not supported";
+//   }
+// }
