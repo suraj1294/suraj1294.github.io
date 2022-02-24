5cc5e2ae9f826052c762df1ce7869b08dfad56b2
spatil_ftdr <suraj.patil@frontdoorhome.com>
2022-02-24 19:16:21 +0530

    remove commented code

--- cookie-test/script.js
+++ cookie-test/script.js
@@ -5,16 +5,3 @@ if (typeof window === "undefined") {
 } else {
   localStorage.setItem("some1", "test1");
 }
-
-//window.localStorage.setItem("some", "test");
-
-// if (window.parent) {
-//   debugger;
-//   if (/thirdparty=yes/.test(document.cookie)) {
-//     window.parent.postMessage("MM:3PCsupported", "*");
-//     document.getElementById("result").innerHTML = "supported";
-//   } else {
-//     window.parent.postMessage("MM:3PCunsupported", "*");
-//     document.getElementById("result").innerHTML = "not supported";
-//   }
-// }
